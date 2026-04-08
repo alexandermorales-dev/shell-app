85c42724a54f7b5089db149dcaacf4df7b6c7bf8
Analista TED 02 <analistated02@shavzla.com>
2026-04-08 15:48:10 -0400

    fix: replace optional catch-all with explicit sidebar slot routes

---
+++ app/(shell)/@sidebar/capacitacion/[...path]/page.tsx
@@ -0,0 +1,5 @@
+import { AppSidebar } from "@/components/shell/AppSidebar";
+
+export default function CapacitacionSidebarSubPath() {
+  return <AppSidebar />;
+}
---
+++ app/(shell)/@sidebar/negocios/[...path]/page.tsx
@@ -0,0 +1,5 @@
+import { AppSidebar } from "@/components/shell/AppSidebar";
+
+export default function NegociosSidebarSubPath() {
+  return <AppSidebar />;
+}
